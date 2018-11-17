35565eaf9e197e80c04da9a5e7c9cd8d5f29727e
WangChen <wangchen100@163.com>
2018-11-17 23:31:30 +0800

    delete sudo in front of pip install .

--- vizdoom.sh
+++ vizdoom.sh
@@ -5,40 +5,14 @@
 #2018/11/16 wangchen v0.1
 
 
-# vizdoom
-zdoom_dependencies_install(){
-    # ZDoom dependencies
-    sudo apt-get install build-essential zlib1g-dev libsdl2-dev libjpeg-dev \
-    nasm tar libbz2-dev libgtk2.0-dev cmake git libfluidsynth-dev libgme-dev \
-    libopenal-dev timidity libwildmidi-dev unzip
-
-    # Boost libraries
-    sudo apt-get install libboost-all-dev
-
-    # Python 2 dependencies
-    #sudo apt-get install python-dev python-pip
-    #pip install numpy
-    # or install Anaconda 2 and add it to PATH
-
-    # Python 3 dependencies
-    sudo apt-get install python3-dev python3-pip
-    pip3 install numpy
-    # or install Anaconda 3 and add it to PATH
-
-    # Lua binding dependencies
-    sudo apt-get install liblua5.1-dev
-    # Lua shipped with Torch can be used instead, so it isn't needed if installing via LuaRocks
-
-    # Julia dependencies
-    sudo apt-get install julia
-    julia
-    Pkg.add("CxxWrap")
-}
-
 #vizdoom
 vizdoom_install(){
-    sudo pip install vizdoom
+    #source ~project/tensorflow/bin/activate
+    #git clone https://github.com/mwydmuch/ViZDoom
+    cd ViZDoom
+    pip install .
+    #deactivate
 }
 
-zdoom_dependencies_install
+
 vizdoom_install
